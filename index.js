73b0c89f15a8c93eeab13dfb5f26810b1a5042ac
Dominik Szymanski <3391674+szymansd@users.noreply.github.com>
2022-08-07 17:12:15 +0200

    Get commit list

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+var { execSync } = require("child_process");
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    const targetBranch = process.env.GITHUB_BASE_REF;
+    const baseBranch = process.env.GITHUB_REF;
+
+    const commitList = execSync(`git log --grep="GA-" --pretty=format:"%s" --no-merges --author-date-order ${targetBranch}...${baseBranch}`);
+    const featurePattern = /GA-\d+/gi;
+    const features = [];
+    const chores = [];
+
+    commitList.forEach((item) => {
+        if(item.match(featurePattern)) {
+            features.push(item);
+        } else {
+            chores.push(item);
+        }
+    });
+
+
 } catch (error) {
     core.setFailed(error.message);
 }
