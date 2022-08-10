29eb8e587870b92b200925e755281a63412b57f7
Dominik Szymanski <3391674+szymansd@users.noreply.github.com>
2022-08-10 09:35:10 +0200

    Make default description nicer

--- dist/index.js
+++ dist/index.js
@@ -8849,14 +8849,6 @@ module.exports = require("assert");
 
 /***/ }),
 
-/***/ 2081:
-/***/ ((module) => {
-
-"use strict";
-module.exports = require("child_process");
-
-/***/ }),
-
 /***/ 2361:
 /***/ ((module) => {
 
@@ -9012,28 +9004,28 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
-const { execSync } = __nccwpck_require__(2081);
 
 const run = async () => {
     const githubToken = core.getInput('github_token', { required: true });
-    const descriptionTemplate = core.getInput('description_template') || `
-        **Features:**
+    const descriptionTemplate = core.getInput('description_template') ||
+        `# Features:
         {{feature_commits}}
-        **Chores:**
+        \n# Chores:
         {{chores_commits}}
-    `;
+        `;
     const titleTemplate = core.getInput('title_template') || `Deployment {{date}}`;
 
     const octokit = github.getOctokit(githubToken);
-    const params = {
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-    };
 
-    const targetBranch = process.env.GITHUB_BASE_REF;
     const baseBranch = process.env.GITHUB_REF;
 
     const [_, pull, pullNumber] = baseBranch.split('/');
+
+    const params = {
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: pullNumber,
+    };
 
     if (pull !== 'pull') {
         throw new Error('This is not a Pull Request');
@@ -9041,15 +9033,18 @@ const run = async () => {
 
     const featurePattern = core.getInput("feature_commit_pattern");
 
-    const commitList = execSync(`git log --pretty=format:"%s" --no-merges --author-date-order ${targetBranch}...${baseBranch}`);
+    const { data } = await octokit.request(`GET /repos/${params.owner}/${params.repo}/pulls/${params.pull_number}/commits`, {
+        ...params,
+    })
     const features = [];
     const chores = [];
+    console.log(data);
 
-    commitList.forEach((item) => {
-        if(item.match(featurePattern)) {
-            features.push(item);
+    data.forEach(({ commit }) => {
+        if(commit.message.match(featurePattern)) {
+            features.push(commit.message);
         } else {
-            chores.push(item);
+            chores.push(commit.message);
         }
     });
 
@@ -9058,16 +9053,17 @@ const run = async () => {
 
     const url = `/repos/${params.owner}/${params.repo}/pulls/${pullNumber}`;
 
-    params.description = descriptionTemplate
+    params.body = descriptionTemplate
         .replace('{{feature_commits}}', features.reduce((prev, curr) => prev += `\n- ${curr}`, ''))
         .replace('{{chores_commits}}', chores.reduce((prev, curr) => prev += `\n- ${curr}`, ''));
-    params.title = titleTemplate.replace('{{date}}', `${date.getDate()} ${month}`);
 
+    params.title = titleTemplate.replace('{{date}}', `${date.getDate()} ${month}`);
+    console.log(params)
     await octokit.request(`PATCH ${url}`, params);
 }
 
 run().then(() => {
-        core.info('Pull request updated succesfully');
+        core.info('Pull request updated successfully');
     })
     .catch((e) => {
         core.setFailed(e.message);
--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { execSync } = require("child_process");
 
 const run = async () => {
     const githubToken = core.getInput('github_token', { required: true });
-    const descriptionTemplate = core.getInput('description_template') || `
-        **Features:**
+    const descriptionTemplate = core.getInput('description_template') ||
+        `# Features:
         {{feature_commits}}
-        **Chores:**
+        \n# Chores:
         {{chores_commits}}
-    `;
+        `;
     const titleTemplate = core.getInput('title_template') || `Deployment {{date}}`;
 
     const octokit = github.getOctokit(githubToken);
-    const params = {
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-    };
 
-    const targetBranch = process.env.GITHUB_BASE_REF;
     const baseBranch = process.env.GITHUB_REF;
 
     const [_, pull, pullNumber] = baseBranch.split('/');
+
+    const params = {
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: pullNumber,
+    };
 
     if (pull !== 'pull') {
         throw new Error('This is not a Pull Request');
@@ -29,15 +29,18 @@ const run = async () => {
 
     const featurePattern = core.getInput("feature_commit_pattern");
 
-    const commitList = execSync(`git log --pretty=format:"%s" --no-merges --author-date-order ${targetBranch}...${baseBranch}`);
+    const { data } = await octokit.request(`GET /repos/${params.owner}/${params.repo}/pulls/${params.pull_number}/commits`, {
+        ...params,
+    })
     const features = [];
     const chores = [];
+    console.log(data);
 
-    commitList.forEach((item) => {
-        if(item.match(featurePattern)) {
-            features.push(item);
+    data.forEach(({ commit }) => {
+        if(commit.message.match(featurePattern)) {
+            features.push(commit.message);
         } else {
-            chores.push(item);
+            chores.push(commit.message);
         }
     });
 
@@ -46,16 +49,17 @@ const run = async () => {
 
     const url = `/repos/${params.owner}/${params.repo}/pulls/${pullNumber}`;
 
-    params.description = descriptionTemplate
+    params.body = descriptionTemplate
         .replace('{{feature_commits}}', features.reduce((prev, curr) => prev += `\n- ${curr}`, ''))
         .replace('{{chores_commits}}', chores.reduce((prev, curr) => prev += `\n- ${curr}`, ''));
-    params.title = titleTemplate.replace('{{date}}', `${date.getDate()} ${month}`);
 
+    params.title = titleTemplate.replace('{{date}}', `${date.getDate()} ${month}`);
+    console.log(params)
     await octokit.request(`PATCH ${url}`, params);
 }
 
 run().then(() => {
-        core.info('Pull request updated succesfully');
+        core.info('Pull request updated successfully');
     })
     .catch((e) => {
         core.setFailed(e.message);
